1eb24f19bfa7afea1c83cab0640d465f6b14ae0c
andregalvao9 <137183044+andregalvao9@users.noreply.github.com>
2024-02-27 10:20:34 -0300

    Add files via upload

---
+++ CENSI.php
@@ -0,0 +1,12 @@
+php
+<?php
+header("Content-Type: application/json");
+
+$chave_1="TQurL2r5mR6quMi94Ka1JTQOMPqRjAlw";
+$hashChave_2= sha1("NvzGSw6LzUDHojLcJps3cIuYxbUI5U6A");
+$utc_tz = new DateTimeZone('UTC');
+$now_dt = new DateTime("now",$utc_tz);
+$token = hash_hmac('sha1',$now_dt->format("YmdHi").$chave_1,$hashChave_2);
+
+echo json_encode(["token" => $token]);
+?>
